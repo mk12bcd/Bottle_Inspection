8e032a987fd3698e8a1d858d43d4e36dbd9f29bf
mk12bcd <mubarakolawale@gmail.com>
2026-03-17 12:34:24 +0200

    Add scripts and images

--- src/train_classifier.py
+++ src/train_classifier.py
@@ -3,9 +3,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import joblibgit add .
+import joblib
 git commit -m "Add scripts and images"
-git push origin mai
+git push origin main
 # ========================
 # Step 1: Load preprocessed data
 # ========================
