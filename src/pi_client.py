8a70654c17584597cebedc79cc04ecaa08011c4e
mk12bcd <mubarakolawale@gmail.com>
2026-04-19 23:48:54 +0200

    new pi script

--- src/pi_client.py
+++ src/pi_client.py
@@ -1,11 +1,15 @@
 import socket
+import time
 
 pc_ip = "192.168.1.1"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((pc_ip, 5000))
 
-client.send("Hello from Pi".encode())
+while True:
+    client.send("Hello from Pi".encode())
 
-response = client.recv(1024).decode()
-print("PC says:", response)
+    response = client.recv(1024).decode()
+    print("PC says:", response)
+
+    time.sleep(2)
