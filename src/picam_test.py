17d4ee530208411b6dec81b33700db81c6607309
mk12bcd <mubarakolawale@gmail.com>
2026-04-20 00:11:02 +0200

    new code

---
+++ src/picam_test.py
@@ -0,0 +1,13 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    print(ret)
+
+    if ret:
+        cv2.imshow("test", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
